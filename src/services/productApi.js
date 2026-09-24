b622da7a5c40909f7e924e2062a4a4a38b7162a5
M.Spinks <mikespinks0401@gmail.com>
2026-09-24 09:17:04 -0500

    Connect produt service to remote API

--- src/services/productApi.js
+++ src/services/productApi.js
@@ -1,7 +1,21 @@
 const PRODUCT_URL='https://dummyjson.com/products?limit=30';
 export async function getProducts(){
+  try{
   // TODO 1: await fetch(PRODUCT_URL)
+    const response = await fetch(PRODUCT_URL);
   // TODO 2: check response.ok and throw Error if false
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
   // TODO 3: const data = await response.json()
+    const data = await response.json();
   // TODO 4: return data.products
+    return data.products;
+  } catch (error){
+    console.error('failed to fetch products:', error);
+    throw error;
+  } finally {
+    console.log('getProducts finished');
+  }
 }
